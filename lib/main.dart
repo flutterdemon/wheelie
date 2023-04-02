24bed701ef093f1ffafe57571237173cc47079a4
flutterdemon <flutterdemon25@gmail.com>
2023-04-02 23:12:07 +0300

    added Firebase and a sign in page

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'package:flutter/services.dart';
 import 'package:mpesa_flutter_plugin/mpesa_flutter_plugin.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:wheelie/Screens/homeScreen.dart';
 import 'package:wheelie/Screens/splashscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 late SharedPreferences prefs;
 
-void main() async {
+Future<void> main() async {
   await dotenv.load(fileName: '.env');
   String consumerKey = dotenv.get('MPESA_CONSUMER_KEY');
   String consumerSecret = dotenv.get('MPESA_CONSUMER_SECRET');
@@ -15,6 +18,7 @@ void main() async {
   MpesaFlutterPlugin.setConsumerSecret(consumerSecret);
   WidgetsFlutterBinding.ensureInitialized();
   prefs = await SharedPreferences.getInstance();
+  await Firebase.initializeApp();
 
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
@@ -28,16 +32,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Wheelie',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            primaryColor: Color(0xFF00c4cc),
-            fontFamily: "Lato",
-            textTheme: TextTheme(
-                headline5: TextStyle(
-                    color: Colors.black,
-                    fontSize: 36.0,
-                    fontFamily: "NunitoSans Bold"))),
-        home: SplashScreen());
+      title: 'Wheelie',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: Color(0xFF00c4cc),
+          fontFamily: "Lato",
+          textTheme: TextTheme(
+              headline5: TextStyle(
+                  color: Colors.black,
+                  fontSize: 36.0,
+                  fontFamily: "NunitoSans Bold"))),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomeScreen();
+          } else {
+            return SplashScreen();
+          }
+        },
+      ),
+    );
   }
 }
